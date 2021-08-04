95e9496bcac38e41569f734b0287962e379511eb
nelsua <nlsnenrique@yahoo.com>
2021-08-04 09:34:39 -0700

    Update Instruction.java

--- CultureTags/src/com/company/Instruction.java
+++ CultureTags/src/com/company/Instruction.java
@@ -22,16 +22,4 @@ public class Instruction {
             e.printStackTrace();
         }
     }
-
-    public void writeInstructions(){
-        try {
-            FileWriter myWriter = new FileWriter("//Users/nelsonenriquez/Desktop/instructions.txt");
-            myWriter.write("Files in Java might be tricky, but it is fun enough!");
-            myWriter.close();
-            System.out.println("Successfully wrote to the file.");
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
-    }
 }
